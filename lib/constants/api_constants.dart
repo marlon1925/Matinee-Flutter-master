7f35648f2066a2a1e1690e1ac99879709c7e9159
marlon1925 <marlon.lalangui@epn.edu.ec>
2024-02-02 15:25:53 -0500

    origin

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "4aee278523108aa2d31f1e746a950957";
+const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
